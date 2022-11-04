0b560df2bbda46a85bc33ef9343f3677e0a2184d
Tarun <tarun@Taruns-MacBook-Pro.local>
2022-11-04 12:52:10 +0530

    Created login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,49 @@
+import { TextField, Typography } from "@mui/material";
+import LoadingButton from "@mui/lab/LoadingButton";
+import LoginIcon from "@mui/icons-material/Login";
+import { useState } from "react";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+  };
+
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-gray-50">
+      <form
+        onSubmit={handleSubmit}
+        className="py-5 px-10 flex items-start justify-start flex-col space-y-3 w-[25%] rounded-md shadow bg-white"
+      >
+        <Typography variant="h6">Sign in to you account</Typography>
+        <TextField
+          type="text"
+          variant="outlined"
+          label="Username"
+          required
+          fullWidth
+        />
+        <TextField
+          variant="outlined"
+          label="Password"
+          required
+          fullWidth
+          type="password"
+        />
+        <LoadingButton
+          loading={isLoading}
+          loadingPosition="start"
+          variant="contained"
+          type="submit"
+          startIcon={<LoginIcon />}
+        >
+          Login
+        </LoadingButton>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
